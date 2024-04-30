3c6d4153b97f07d1a9457294d4b002c0649c2915
Vahalcadi <davidepetrone04@gmail.com>
2024-04-30 18:22:51 +0200

    Added pokemon sprites, working on SO

---
+++ Assets/ScriptableObjects/Scripts/PokemonSO.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Type
+{
+    DARK,
+    FIGHTING,
+    GHOST,
+    PSYCHIC,
+    FAIRY,
+    DRAGON,
+    FIRE,
+    GRASS,
+    STEEL,
+    NORMAL,
+    ROCK,
+    ICE,
+    ELECTRIC,
+    FLYING,
+    POISON,
+    WATER,
+    BUG
+}
+
+[CreateAssetMenu]
+public class PokemonSO : ScriptableObject
+{
+    new public string name;
+
+    public Sprite frontSprite;
+    public Sprite backSprite;
+
+    public Type type1;
+    public Type type2;
+
+    public int maxHp;
+    public int attack;
+    public int defence;
+    public int specialAttack;
+    public int specialDefence;
+    public int speed;
+}
