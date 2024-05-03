a1c51be102e5a135b5e78db4799eaa40c4104176
Vahalcadi <davidepetrone04@gmail.com>
2024-05-04 00:24:44 +0200

    refactoring

--- Assets/Script/Battle/BattleUnit.cs
+++ Assets/Script/Battle/BattleUnit.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class BattleUnit : MonoBehaviour
 {
     [SerializeField] bool isPlayerUnit;
+    [SerializeField] BattleHud hud;
 
+    public BattleHud Hud { get { return hud; } }
+    public bool IsPlayerUnit { get { return isPlayerUnit; } }
     public Pokemon Pokemon { get; set; }
 
     public void Setup(Pokemon pokemon)
@@ -17,6 +20,8 @@ public class BattleUnit : MonoBehaviour
             GetComponent<Image>().sprite = Pokemon.PokemonSO.backSprite;
         else
             GetComponent<Image>().sprite = Pokemon.PokemonSO.frontSprite;
+
+        hud.SetData(pokemon);
     }
 
     public Image GetImageComponent()
